216e20b3f11ca08a39f247d894526296df74181b
SweJax <59422603+SweJax@users.noreply.github.com>
2024-10-05 17:10:27 +0200

    Fixed: Added a new feature that allows users to calculate the length when the product type, diameter, and thickness are provided.

---
+++ length.js
@@ -0,0 +1,118 @@
+
+function tab2_visibility(choosed_product) {
+    const hidden_elements = ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'];
+    const inputs = ['totalSpoolDiameter', 'thickness2'];
+    hidden_elements.forEach(el => document.getElementById(el).style.display = 'none');
+    inputs.forEach(el => document.getElementById(el).value = '');
+
+    switch(choosed_product) {
+        case 'no_option':
+            layer = 0
+            welded = false;
+            tab2_reset()
+            break;
+        case 'foil_option':
+            layer = 2; // 1 layers * 2 sides
+            welded = false;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        case 'tube_option':
+            layer = 4; // 2 layers * 2 sides
+            welded = false;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        case 'folded_tube_option':
+            layer = 8; // 4 layers * 2 sides
+            welded = false;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        case 'huv_option':
+            layer = 4; // 2 layers * 2 sides
+            welded = true;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        case 'folded_huv_option':
+            layer = 8; // 4 layers * 2 sides
+            welded = true;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        case 'sheet_option':
+            layer = 2; // 1 layers * 2 sides
+            welded = true;
+            ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'block');
+            break;
+        default:
+            console.log("Unknown selection.");
+    }
+}
+
+function tab2_submit() {
+    const spool_diameter = document.getElementById("spool_diameter2").value;
+    const thickness =  document.getElementById("thickness2").value.replace(',', '.');
+    const total_spool_diameter = document.getElementById("totalSpoolDiameter").value;
+    console.log(total_spool_diameter)
+
+    try {
+        validate_inputs(spool_diameter, thickness, total_spool_diameter)
+        calculate_length(spool_diameter, total_spool_diameter, thickness);
+    } catch (error) {
+        document.getElementById('result_label2').innerHTML = error;
+        document.getElementById('count_label2').innerHTML = '';
+    }
+}
+
+function validate_inputs(spool_diameter, thickness, total_spool_diameter) {
+    const MAXLENGTH = 10000;
+    const MAXTHICKNESS = 50;
+
+    if (thickness > MAXTHICKNESS) {
+        throw new Error("Orimlig tjocklek");
+    }
+
+    if (total_spool_diameter > MAXLENGTH) {
+        throw new Error("Orimlig diameter");
+    }
+
+    if (!isInteger(spool_diameter) || !isInteger(total_spool_diameter)) {
+        throw new Error("Otillåtna värden");
+    }
+
+// Validate length to allow decimals
+    if (!isNumber(thickness)) {
+        throw new Error("Otillåtna värden");
+    }
+
+    return true;
+}
+
+function calculate_length(spool_diameter, total_spool_diameter, thickness) {
+    let length = 0;
+    let current_diameter = parseFloat(spool_diameter);
+    const added_thickness = thickness * layer; // Total amount of mm added to the diameter per iteration
+    let count = 0;
+    while (current_diameter < parseFloat(total_spool_diameter)) {
+        current_diameter += added_thickness;
+        length += current_diameter * Math.PI;
+        count++;
+    }
+    const length_m = (length / 1000).toFixed(2);
+    const current_circumference = `${((current_diameter * Math.PI) / 1000).toFixed(2)}`
+    document.getElementById('result_label2').innerHTML = 'Längd: ' + length_m + ' ± '  + current_circumference + ' m';
+    document.getElementById('count_label2').innerHTML = 'Antal varv: ' + count.toString() + ' st';
+}
+
+function tab2_reset() {
+    const productOptionSelect = document.getElementById('productOption2');
+    productOptionSelect.value = 'no_option';
+    // Set the spool diameter to 90
+    const spoolDiameterInput = document.getElementById('spool_diameter2');
+    spoolDiameterInput.value = 90;
+
+    const inputs = ['totalSpoolDiameter', 'thickness2'];
+    inputs.forEach(el => document.getElementById(el).value = '');
+
+    ['totalSpoolDiameterWrapper', 'thicknessWrapper2', 'submitWrapper2'].forEach(el => document.getElementById(el).style.display = 'none');
+    document.getElementById('result_label2').innerHTML = '';
+    document.getElementById('count_label2').innerHTML = '';
+
+}
--- scripts.js
+++ scripts.js
@@ -1,243 +1,275 @@
-function adjustVisibility(selectedOption) {
+let layer = 0;
+let welded = false;
+
+function adjustVisibility(choosed_product) {
             // Hide all elements initially
-            const elements = ['lengthWrapper', 'thicknessWrapper', 'huvAmountWrapper', 'huvLengthWrapper', 'sheetAmountWrapper', 'sheetLengthWrapper'];
+            const hidden_elements = ['lengthWrapper', 'thicknessWrapper', 'huvAmountWrapper', 'huvLengthWrapper', 'sheetAmountWrapper', 'sheetLengthWrapper', 'submitWrapper'];
             const inputs = ['length', 'thickness', 'huv_amount', 'huv_length', 'sheet_amount', 'sheet_length'];
-            elements.forEach(el => document.getElementById(el).style.display = 'none');
+            hidden_elements.forEach(el => document.getElementById(el).style.display = 'none');
             inputs.forEach(el => document.getElementById(el).value = '');
-            document.getElementById('submitWrapper').style.display = 'none'; // Make sure to handle submit button visibility correctly.
+
+            document.getElementById('spoolDiameterWrapper').style.display = 'block'; // Bobin diameter always visible.
 
             document.getElementById('result_label').innerHTML = '';
             document.getElementById('count_label').innerHTML = '';
 
             // Show elements based on selection
-            document.getElementById('spoolDiameterWrapper').style.display = 'block'; // Bobin diameter always visible.
-            switch(selectedOption) {
+            switch(choosed_product) {
                 case 'no_option':
+                    layer = 0
+                    welded = false;
+                    tab1_reset()
                     break;
                 case 'foil_option':
+                    layer = 2; // 1 layers * 2 sides
+                    welded = false;
+                    ['lengthWrapper', 'thicknessWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
+                    break;
                 case 'tube_option':
+                    layer = 4; // 2 layers * 2 sides
+                    welded = false;
+                    ['lengthWrapper', 'thicknessWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
+                    break;
                 case 'folded_tube_option':
+                    layer = 8; // 4 layers * 2 sides
+                    welded = false;
                     ['lengthWrapper', 'thicknessWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
                     break;
                 case 'huv_option':
+                    layer = 4; // 2 layers * 2 sides
+                    welded = true;
                     ['thicknessWrapper', 'huvAmountWrapper', 'huvLengthWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
                     break;
                 case 'folded_huv_option':
+                    layer = 8; // 4 layers * 2 sides
+                    welded = true;
                     ['thicknessWrapper', 'huvAmountWrapper', 'huvLengthWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
                     break;
                 case 'sheet_option':
+                    layer = 2; // 1 layers * 2 sides
+                    welded = true;
                     ['thicknessWrapper', 'sheetAmountWrapper', 'sheetLengthWrapper', 'submitWrapper'].forEach(el => document.getElementById(el).style.display = 'block');
-                    break;
-            }
-        }
-
-        function m_to_mm(length) {
-            return length * 1000;
-        }
-
-        function amount_to_length(amount, product_length) {
-            return amount * product_length;
-        }
-
-        function handleOption() {
-            let input = 0;
-            try {
-                input = getInputValues()
-            } catch(e) {
-                document.getElementById('result_label').innerHTML = e.message;
-                document.getElementById('count_label').innerHTML = '';
-                return null;
-            }
-            
-            let layer = 0;
-            let length = 0;
-            document.getElementById('result_label').innerHTML = '';
-            document.getElementById('count_label').innerHTML = '';
-
-
-            switch (input.selectedValue) {
-                case "no_option":
-                    console.log("No product selected.");
-                    break;
-                case "foil_option":
-                    console.log("Folie selected.");
-                    layer = 2; // 1 layers * 2 sides
-                    length = m_to_mm(input.length_m);
-                    break;
-                case "tube_option":
-                    console.log("Slang selected.");
-                    layer = 4; // 2 layers * 2 sides
-                    length = m_to_mm(input.length_m);
-                    break;
-                case "folded_tube_option":
-                    console.log("Slang m. invik selected.");
-                    layer = 8; // 4 layers * 2 sides
-                    length = m_to_mm(input.length_m);
-                    break;
-                case "huv_option":
-                    console.log("Pallhuv selected.");
-                    layer = 4; // 2 layers * 2 sides
-                    length = amount_to_length(input.huvAmount, input.huvLength)
-                    break;
-                case "folded_huv_option":
-                    console.log("Pallhuv m. invik selected.");
-                    layer = 8; // 4 layers * 2 sides
-                    length = amount_to_length(input.huvAmount, input.huvLength)
-                    break;
-                case "sheet_option":
-                    console.log("Toppark selected.");
-                    layer = 2; // 1 layers * 2 sides
-                    length = amount_to_length(input.sheetAmount, input.sheetLength)
                     break;
                 default:
                     console.log("Unknown selection.");
             }
-
-            const resultObj = {
-                spoolDiameter: input.spoolDiameter,
-                length: length,
-                thickness: input.thickness
-            };
-
-            if(!validateNoneZero(resultObj)) {
-                document.getElementById('result_label').innerHTML = "Error: Tomt fält";
-                document.getElementById('count_label').innerHTML = '';
-                return null;  
-            }
-
-            if(input.spoolDiameter && input.thickness && length && layer) {
-                const result = calculateSpoolDiameter(length, input.thickness, input.spoolDiameter, layer )
-                console.log(result.result_string);
-                console.log(result.result);
-                console.log(result.count);
-            }
         }
 
-        function getInputValues() {
-            const MAXLENGTH = 10000000000;
-            const MAXTHICKNESS = 500;
+function m_to_mm(length) {
+    return length * 1000;
+}
 
-            const selectedValue = document.getElementById('productOption').value || "0";
-            let spoolDiameter = document.getElementById('spool_diameter').value || "0";
-            const length_m = document.getElementById('length').value || "0";
-            const thickness = document.getElementById('thickness').value || "0";
-            const huvAmount = document.getElementById('huv_amount').value || "0";
-            const huvLength = document.getElementById('huv_length').value || "0";
-            const sheetAmount = document.getElementById('sheet_amount').value || "0";
-            const sheetLength = document.getElementById('sheet_length').value || "0";
+function amount_to_length(amount, product_length) {
+    return amount * product_length;
+}
+
+function tab1_submit() {
+    let input = 0;
+    try {
+        input = getInputValues()
+    } catch (e) {
+        document.getElementById('result_label').innerHTML = e.message;
+        document.getElementById('count_label').innerHTML = '';
+        return null;
+    }
+
+    document.getElementById('result_label').innerHTML = '';
+    document.getElementById('count_label').innerHTML = '';
+
+    let length = 0
+    if (welded) {
+        length = amount_to_length(input.amount, input.length)
+    }else {
+        length = m_to_mm(input.length_m);
+    }
+
+    const resultObj = {
+        spoolDiameter: input.spoolDiameter,
+        length: length,
+        thickness: input.thickness
+    };
+
+    if (!validateNoneZero(resultObj)) {
+        document.getElementById('result_label').innerHTML = "Error: Tomt fält";
+        document.getElementById('count_label').innerHTML = '';
+        return null;
+    }
+
+    if (input.spoolDiameter && input.thickness && length && layer) {
+        calculate_diameter(length, input.thickness, input.spoolDiameter)
+    }else {
+        console.log("Value Error")
+    }
+}
+
+function getInputValues() {
+    const MAXLENGTH = 10000000000;
+    const MAXTHICKNESS = 50;
+
+    const selectedValue = document.getElementById('productOption').value || "0";
+    let spoolDiameter = document.getElementById('spool_diameter').value || "0";
+    const length_m = document.getElementById('length').value || "0";
+    const thickness = document.getElementById('thickness').value || "0";
+    let amount = 0;
+    let length = 0;
+    if(selectedValue.includes("huv")) {
+        amount = document.getElementById('huv_amount').value || "0";
+        length = document.getElementById('huv_length').value || "0";
+    }else {
+        amount = document.getElementById('sheet_amount').value || "0";
+        length = document.getElementById('sheet_length').value || "0";
+    }
 
 
+// Validate integer values
+    if (!isInteger(spoolDiameter) || !isInteger(amount) || !isInteger(length)) {
+        console.log('One or more fields are not valid integers.');
+        throw new Error("Otillåtna värden");
+    }
 
-            // Validate integer values
-            if (!isInteger(spoolDiameter) || !isInteger(huvAmount) || !isInteger(huvLength) || !isInteger(sheetAmount) || !isInteger(sheetLength)) {
-                console.log('One or more fields are not valid integers.');
-                throw new Error("Otillåtna värden");
-            }
+// Validate length to allow decimals
+    if (!isNumber(length_m) || !isNumber(thickness)) {
+        console.log('One or more fields are not valid numbers.');
+        throw new Error("Otillåtna värden");
+    }
 
-            // Validate length to allow decimals
-            if (!isNumber(length_m) || !isNumber(thickness)) {
-                console.log('One or more fields are not valid numbers.');
-                throw new Error("Otillåtna värden");
-            }
+    let new_length_m = length_m.replace(',', '.');
+    let new_thickness = thickness.replace(',', '.');
 
-            let new_length_m = length_m.replace(',', '.');
-            let new_thickness = thickness.replace(',', '.');
+    const resultObject = {
+        selectedValue: selectedValue,
+        spoolDiameter: parseInt(spoolDiameter),
+        length_m: parseFloat(new_length_m),
+        thickness: parseFloat(new_thickness),
+        amount: parseInt(amount),
+        length: parseInt(length)
+    };
 
-            const resultObject = {
-                selectedValue: selectedValue,
-                spoolDiameter: parseInt(spoolDiameter),
-                length_m: parseFloat(new_length_m),
-                thickness: parseFloat(new_thickness),
-                huvAmount: parseInt(huvAmount),
-                huvLength: parseInt(huvLength),
-                sheetAmount: parseInt(sheetAmount),
-                sheetLength: parseInt(sheetLength)
-            };
+    if (thickness > MAXTHICKNESS) {
+        throw new Error("Orimlig tjocklek");
+    }
 
-            if (thickness > MAXTHICKNESS) {
-                throw new Error("Orimlig tjocklek");
-            }
+    if ((amount * length > MAXLENGTH) || m_to_mm(length_m) > MAXLENGTH) {
+        throw new Error("Orimligt lång");
+    }
 
-            if ((huvAmount * huvLength > MAXLENGTH) || (sheetAmount * sheetLength) > MAXLENGTH || m_to_mm(length_m) > MAXLENGTH) {
-                throw new Error("Orimligt lång");
-            }
-            
-            if (validateAllPositive(resultObject)) {
-                return resultObject;
-            } else {
-                throw new Error("Negativa värden är ej tillåtna");
-            }
+    if (validateAllPositive(resultObject)) {
+        return resultObject;
+    } else {
+        throw new Error("Negativa värden är ej tillåtna");
+    }
+}
+
+// Helper functions to check for integers and numeric values
+function isInteger(value) {
+    const num = parseInt(value, 10);
+    return !isNaN(num) && (num.toString() === value);
+}
+
+function isNumber(value) {
+    const num = parseFloat(value);
+    return !isNaN(num) && isFinite(num);
+}
+
+function validateAllPositive(obj) {
+    for (let key in obj) {
+        if (key !== 'selectedValue' && obj[key] < 0) {
+            return false;
         }
+    }
+    return true;
+}
 
-        // Helper functions to check for integers and numeric values
-        function isInteger(value) {
-            const num = parseInt(value, 10);
-            return !isNaN(num) && (num.toString() === value);
+function validateNoneZero(obj) {
+    for (let key in obj) {
+        if (key !== 'selectedValue' && obj[key] <= 0) {
+            return false;
         }
+    }
+    return true;
+}
 
-        function isNumber(value) {
-            const num = parseFloat(value);
-            return !isNaN(num) && isFinite(num);
-        }
+function validateLength(obj) {
 
-        function validateAllPositive(obj) {
-            for (let key in obj) {
-                if (key !== 'selectedValue' && obj[key] < 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        function validateNoneZero(obj) {
-            for (let key in obj) {
-                if (key !== 'selectedValue' && obj[key] <= 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        function validateLength(obj) {
-
-        }
+}
 
 
-        function calculateSpoolDiameter(length, thickness, spoolDiameter, layer) {
-            const totalMm = thickness * layer; // Total amount of mm added to the diameter per iteration
-            const resultDict = {};
-            const spoolDiameterCm = spoolDiameter / 10; // mm -> cm
-            let lengthSum = 0;
-            let count = 0;
+function calculate_diameter(length, thickness, spoolDiameter) {
+    const totalMm = thickness * layer; // Total amount of mm added to the diameter per iteration
+    const resultDict = {};
+    const spoolDiameterCm = spoolDiameter / 10; // mm -> cm
+    let lengthSum = 0;
+    let count = 0;
 
-            while (lengthSum + spoolDiameter < length) {
-                const term = (spoolDiameter + totalMm * count) * Math.PI;
-                lengthSum += term;
-                count += 1;
-            }
+    while (lengthSum + spoolDiameter < length) {
+        const term = (spoolDiameter + totalMm * count) * Math.PI;
+        lengthSum += term;
+        count += 1;
+    }
 
-            resultDict.resultStr = `${((count * totalMm) / 10 + spoolDiameterCm).toFixed(2)} cm`;
-            resultDict.result = (count * totalMm) / 10 + spoolDiameterCm;
-            resultDict.count = count;
+    resultDict.resultStr = `${((count * totalMm) / 10 + spoolDiameterCm).toFixed(2)} cm`;
+    resultDict.result = (count * totalMm) / 10 + spoolDiameterCm;
+    resultDict.count = count;
 
-            document.getElementById('result_label').innerHTML = 'Diameter: ' + resultDict.resultStr;
-            document.getElementById('count_label').innerHTML = 'Antal varv: ' + resultDict.count;
+    document.getElementById('result_label').innerHTML = 'Diameter: ' + resultDict.resultStr;
+    document.getElementById('count_label').innerHTML = 'Antal varv: ' + resultDict.count;
 
-            return {
-                result_string: resultDict.resultStr,
-                result: resultDict.result,
-                count: resultDict.count
-            };
-        }
-        
-        // Page
-        window.onload = function() {
-            // Set the spool diameter to 90
-            var spoolDiameterInput = document.getElementById('spool_diameter');
-            spoolDiameterInput.value = 90;
+    return {
+        result_string: resultDict.resultStr,
+        result: resultDict.result,
+        count: resultDict.count
+    };
+}
 
-            // Set the dropdown option to 'no_option'
-            var productOptionSelect = document.getElementById('productOption');
-            productOptionSelect.value = 'no_option';
-            adjustVisibility('no_option');
-        };
+// Page
+window.onload = function () {
+    reset_all()
+};
+
+function openTab(element, tabName) {
+    reset_all();
+    // Hide all tab content
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the "active" class from all tablinks
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Show the selected tab content
+    document.getElementById(tabName).style.display = "block";
+
+    // Add the "active" class to the clicked tab
+    element.classList.add("active");
+}
+function reset_all() {
+    tab1_reset()
+    tab2_reset()
+}
+
+function tab1_reset() {
+    // Set the spool diameter to 90
+    const spoolDiameterInput = document.getElementById('spool_diameter');
+    spoolDiameterInput.value = 90;
+
+    // Hide all elements initially
+    const wrappers = ['lengthWrapper', 'thicknessWrapper', 'huvAmountWrapper', 'huvLengthWrapper', 'sheetAmountWrapper', 'sheetLengthWrapper'];
+    const inputs = ['length', 'thickness', 'huv_amount', 'huv_length', 'sheet_amount', 'sheet_length'];
+    const productOptionSelect = document.getElementById('productOption');
+    wrappers.forEach(el => document.getElementById(el).style.display = 'none');
+    inputs.forEach(el => document.getElementById(el).value = '');
+    document.getElementById('submitWrapper').style.display = 'none'; // Make sure to handle submit button visibility correctly.
+    productOptionSelect.value = 'no_option';
+    document.getElementById('result_label').innerHTML = '';
+    document.getElementById('count_label').innerHTML = '';
+
+}
+
+
+
+
+
